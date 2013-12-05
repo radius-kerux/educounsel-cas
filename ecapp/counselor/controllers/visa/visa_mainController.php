0f8a0dca23b61f516734f1599b764716952499d2
callmebaby69 <kevin.baisas@yahoo.com>
2013-12-06 01:08:58 +0800

    dummy visa section. management module: to follow

--- ecapp/counselor/controllers/visa/visa_mainController.php
+++ ecapp/counselor/controllers/visa/visa_mainController.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * 
+ *
  * Folder specific main class controller can be set up this way :
  * The name of this main controller should be foldername_mainController
  * Before any requested controller within a folder is called, SPINE will call first the folder specific main controller if it exists
@@ -10,15 +10,16 @@
  */
 class visa_mainController extends Spine_SuperController implements Spine_MainInterface
 {
-	
-	public function main()
-	{
-	}
-	
-	//------------------------------------------------------------------------------------
-	
-	public function end()
-	{
-	}
-	
+
+    public function main()
+    {
+        $this->displayPhtml( 'main_content', 'visa/visa_main' );
+        $this->renderLocalScript('local_bottom_script', 'visa/js/local_bottom_script.js');
+    }
+    //------------------------------------------------------------------------------------
+
+    public function end()
+    {
+    }
+
 }
